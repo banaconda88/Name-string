e950fd7664e92319ee2d935cbb2b32df30dd292e
banaconda88 <bananaconda88@gmail.com>
2021-02-26 21:59:17 -0500

    Add files via upload

---
+++ Name strings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+n = "Alexander"
+print (" There are", len(n), "letters in Alexander.")
+print("The last letter of my name is ", n[8], ".", sep="")
+
